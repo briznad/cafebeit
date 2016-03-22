c2528cbc586f8369f554187a14eaec2a2d1255f5
Brad Mallow <bmallow@poppin.com>
2016-03-22 14:24:20 -0400

    add description stub

--- build/assets/js/defaultData.js
+++ build/assets/js/defaultData.js
@@ -5,7 +5,7 @@
 		return {
 			"name" : "Cafe Beit",
 			"photo" : {
-				"file" : "cortado-pour.png",
+				"file" : "cortado-pour-scaled.png",
 				"attribution" : {
 					"name" : "Brad Mallow",
 					"url" : "http://bradmallow.com",
@@ -19,7 +19,8 @@
 				"postalCode" : "11211"
 			},
 			"hours" : ["Mo,Tu,We,Th,Fr 07:00-19:00", "Sa,Su 08:00-20:00"],
-			"email" : "cafebeit@gmail.com"
+			"email" : "cafebeit@gmail.com",
+			"description" : []
 		};
 	};
 
